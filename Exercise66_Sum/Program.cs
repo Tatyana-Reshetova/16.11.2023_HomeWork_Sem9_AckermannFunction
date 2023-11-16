ec3427ba6c87b3dae5694198271c4995d236c028
Tatyana_Reshetova <vlasenko2205@gmail.com>
2023-11-16 14:20:47 +0300

    Сумма чисел в промежутке

---
+++ Exercise66_Sum/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 66: Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите M: ");
+int m = int.Parse(ReadLine());
+Write("Введите N: ");
+int n = int.Parse(ReadLine());
+int sum = 0;
+
+WriteLine(SumNumbers(m, n));
+
+int SumNumbers(int m, int n)
+{
+    if (m==n) return m;
+    int sum = m + SumNumbers(m+1, n);
+    return sum;
+}
