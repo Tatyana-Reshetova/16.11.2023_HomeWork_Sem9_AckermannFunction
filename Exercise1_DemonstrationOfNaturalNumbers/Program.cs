8eb641a4145886d89e28dee25d78986e6706f0ed
Tatyana_Reshetova <vlasenko2205@gmail.com>
2023-11-16 14:12:19 +0300

    Демонстрация натуральных цисел

---
+++ Exercise1_DemonstrationOfNaturalNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿// **Задача 1:**Задайте значения M и N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке
+//  от  N до M.
+
+// M = 1; N = 5. -> "5, 4, 3, 2, 1"
+
+// M = 4; N = 8. -> "8, 7, 6, 5, 4"
+
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите M: ");
+int m = int.Parse(ReadLine());
+Write("Введите N: ");
+int n = int.Parse(ReadLine());
+
+WriteLine(PrintNumbers(n, m));
+
+string PrintNumbers(int end, int start)
+{
+    if (end == start)
+        return end.ToString();
+    return (end + " " + PrintNumbers(end - 1, start));
+}
